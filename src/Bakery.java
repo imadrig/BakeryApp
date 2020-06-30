c1dfcfff0b50cfef1c89cfb7807c32e1a8ce6511
imadrig <ingridmadrigal1@gmail.com>
2020-06-30 09:47:46 -0400

    Bakery Class

---
+++ src/Bakery.java
@@ -0,0 +1,14 @@
+public class Bakery {
+    private String bakedGood;
+    private int Price;
+    private String expDate;
+    private String dietRestriction;
+    private int quantityInStock;
+
+    public Bakery(){
+    }
+
+    public Bakery (String)
+
+
+}
